a353e45c376692f97fa3c38739a87151a07cf088
SajjanMazumdar <sajjan.mazumdar@gmail.com>
2023-11-21 00:45:46 +0530

    create excel service module with routing

---
+++ src/app/excel-service/excel-service.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExcelServiceRoutingModule } from './excel-service-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExcelServiceRoutingModule
+  ]
+})
+export class ExcelServiceModule { }
