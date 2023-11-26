5e79463ccef906e078f1fc76b9ff93ae0689ba8f
SajjanMazumdar <sajjan.mazumdar@gmail.com>
2023-11-26 19:52:41 +0530

    material module list

---
+++ src/app/material.module.ts
@@ -0,0 +1,63 @@
+// import { NgModule } from "@angular/core";
+
+// import { MatAutocompleteModule } from '@angular/material/autocomplete';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatButtonToggleModule } from '@angular/material/button-toggle';
+// import { MatCheckboxModule } from '@angular/material/checkbox';
+// import { MatChipsModule } from '@angular/material/chips';
+// import { MatNativeDateModule } from '@angular/material/core';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatDialogModule } from '@angular/material/dialog';
+// import { MatDividerModule } from '@angular/material/divider';
+// import { MatExpansionModule } from '@angular/material/expansion';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatListModule } from '@angular/material/list';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { MatPaginatorModule } from '@angular/material/paginator';
+// import { MatProgressBarModule } from '@angular/material/progress-bar';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// import { MatSnackBarModule, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
+// import { MatSortModule } from '@angular/material/sort';
+// import { MatTableModule } from '@angular/material/table';
+// import { MatTabsModule } from '@angular/material/tabs';
+// import { MatTooltipModule } from '@angular/material/tooltip';
+// import { MatRadioModule } from '@angular/material/radio';
+// import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+
+// @NgModule({
+//     exports: [
+//         MatAutocompleteModule,
+//         MatButtonModule,
+//         MatButtonToggleModule,
+//         MatCheckboxModule,
+//         MatChipsModule,
+//         MatNativeDateModule,
+//         MatDatepickerModule,
+//         MatDialogModule,
+//         MatDividerModule,
+//         MatExpansionModule,
+//         MatFormFieldModule,
+//         MatIconModule,
+//         MatInputModule,
+//         MatListModule,
+//         MatMenuModule,
+//         MatPaginatorModule,
+//         MatProgressBarModule,
+//         MatSelectModule,
+//         MatSidenavModule,
+//         MatSlideToggleModule,
+//         MatSnackBarModule,
+//         MatSortModule,
+//         MatTableModule,
+//         MatTabsModule,
+//         MatTooltipModule,
+//         MatRadioModule,
+//         NgxMatSelectSearchModule,
+//     ],
+//     providers: [{ provide: MAT_SNACK_BAR_DATA, useValue: {} }],
+// })
+// export class MaterialModule { }
